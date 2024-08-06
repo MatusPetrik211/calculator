9905f2c972f2cf4b16a9b2c3832acc73e1b53a13
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2024-08-06 22:14:54 +0200

    Add the operate function

--- script.js
+++ script.js
@@ -18,10 +18,18 @@ function remainder(a, b) {
     return a % b;
 }
 
-let a = 6, b = 2;
+let num1 = 0;
+let num2 = 0;
+let operator = '';
 
-console.log(add(a, b));
-console.log(subtract(a, b));
-console.log(multiply(a, b));
-console.log(divide(a, b));
-console.log(remainder(a, b));
+function operate(operator, a, b) {
+    switch(operator) {
+        case '+': add(a, b); break;
+        case '-': subtract(a, b); break;
+        case '*': multiply(a, b); break;
+        case '/': divide(a, b); break;
+        case '%': remainder(a, b); break;
+    }
+}
+
+
